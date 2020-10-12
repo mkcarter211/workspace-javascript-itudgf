af88b8d677e247214dd47adf91ad4f137bc09aab
mkcarter211 <mkcarter@usca.edu>
2020-10-12 13:13:48 -0400

    1 file modified, 1 added

---
+++ js/arith.js
@@ -0,0 +1,8 @@
+//get the element
+
+// element property
+var num1 = ParseInt(document.getElementById('n1').textContent);
+var num2 = ParseInt(document.getElementById('n2').textContent);
+
+
+var c = num1 + num2;
